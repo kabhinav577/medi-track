0e8e1ee799b2d7520e99d1b87eeff23344acdda4
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2024-01-12 11:03:57 +0530

    setup Shadcn/ui for app and work on navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+const Navbar = () => {
+  return (
+    <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-4 dark:bg-gray-800">
+      <nav
+        className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between"
+        aria-label="Global"
+      >
+        <a className="flex-none text-xl font-semibold dark:text-white" href="#">
+          MediTracker
+        </a>
+        <div className="flex flex-row items-center gap-5 mt-5 sm:justify-end sm:mt-0 sm:ps-5">
+          <a
+            className="font-medium text-blue-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+            href="#"
+            aria-current="page"
+          >
+            Home
+          </a>
+          <a
+            className="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+            href="#"
+          >
+            Account
+          </a>
+          <a
+            className="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+            href="#"
+          >
+            Contact
+          </a>
+          <a
+            className="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+            href="#"
+          >
+            About
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
